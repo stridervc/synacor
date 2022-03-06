17cba0f349c6df004a69b0baac4eb675b968d18f
Fred Strauss <fred@strider.co.za>
2022-03-06 09:04:44 +0200

    Stepping

--- src/BrickRunner.hs
+++ src/BrickRunner.hs
@@ -9,14 +9,22 @@ import Brick.Widgets.Core
 import qualified Brick.AttrMap as A
 import qualified Graphics.Vty as V
 
-type UIState = VMState
+data UIState = UIState
+  { vmState :: VMState
+  , output  :: String
+  } deriving (Eq, Show)
 
 drawUI :: UIState -> [ Widget n ]
-drawUI state = [ str "Hello" ]
+drawUI state = [ str $ output state ]
 
 eventHandler :: UIState -> BrickEvent n e -> EventM n (Next UIState)
 eventHandler state (VtyEvent e) = case e of
   V.EvKey (V.KChar 'q') []  -> halt state
+  V.EvKey (V.KChar 's') []  -> do
+                                let (out', vmstate') = stepVM $ vmState state
+                                case out' of
+                                  Just c  -> continue $ state { vmState = vmstate', output = output state <> [c] }
+                                  Nothing -> continue $ state { vmState = vmstate' }
   _                         -> continue state
 eventHandler state _ = continue state
 
@@ -32,4 +40,6 @@ app = App { appDraw         = drawUI
           }
 
 brickRun :: VMState -> IO VMState
-brickRun = defaultMain app
+brickRun state = do
+  uistate' <- defaultMain app $ UIState { vmState = state, output = "" }
+  return $ vmState uistate'
