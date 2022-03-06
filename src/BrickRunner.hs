27cc7e6d827a61543ab6254a6f8168741942f15d
Fred Strauss <fred@strider.co.za>
2022-03-06 08:33:49 +0200

    Basic test Brick

--- src/BrickRunner.hs
+++ src/BrickRunner.hs
@@ -3,6 +3,33 @@ module BrickRunner
   ) where
 
 import VMState
+import Brick.Main
+import Brick.Types
+import Brick.Widgets.Core
+import qualified Brick.AttrMap as A
+import qualified Graphics.Vty as V
+
+type UIState = VMState
+
+drawUI :: UIState -> [ Widget n ]
+drawUI state = [ str "Hello" ]
+
+eventHandler :: UIState -> BrickEvent n e -> EventM n (Next UIState)
+eventHandler state (VtyEvent e) = case e of
+  V.EvKey (V.KChar 'q') []  -> halt state
+  _                         -> continue state
+eventHandler state _ = continue state
+
+attrMap :: UIState -> A.AttrMap
+attrMap s = A.attrMap V.defAttr []
+
+app :: App UIState e ()
+app = App { appDraw         = drawUI
+          , appChooseCursor = \_ _ -> Nothing
+          , appHandleEvent  = eventHandler
+          , appStartEvent   = return
+          , appAttrMap      = attrMap
+          }
 
 brickRun :: VMState -> IO VMState
-brickRun = runVM
+brickRun = defaultMain app
