c42559d570f157c9bfdeaa0687a7524b55cd2e9e
Fred Strauss <fred@strider.co.za>
2022-03-06 09:45:23 +0200

    Show registers

--- src/BrickRunner.hs
+++ src/BrickRunner.hs
@@ -6,6 +6,7 @@ import VMState
 import Brick.Main
 import Brick.Types
 import Brick.Widgets.Core
+import Data.Maybe (maybeToList)
 import qualified Brick.AttrMap as A
 import qualified Graphics.Vty as V
 
@@ -14,18 +15,24 @@ data UIState = UIState
   , output  :: String
   } deriving (Eq, Show)
 
+registersBrick :: UIState -> Widget n
+registersBrick state = vBox [ reg 0, reg 1, reg 2, reg 3, reg 4, reg 5, reg 6, reg 7, regip, reghlt ]
+  where vmstate = vmState state
+        reg r   = str $ "R" <> show r <> "   = " <> show ( vmRegisters vmstate !! r)
+        regip   = str $ "IP   = " <> show ( vmIP vmstate )
+        reghlt  = str $ "HALT = " <> show ( vmHalt vmstate )
+
 drawUI :: UIState -> [ Widget n ]
-drawUI state = [ str $ output state ]
+drawUI state = [ output' <+> registersBrick state]
+  where output' = padRight Max $ str $ output state
 
 eventHandler :: UIState -> BrickEvent n e -> EventM n (Next UIState)
 eventHandler state (VtyEvent e) = case e of
   V.EvKey (V.KChar 'q') []  -> halt state
-  V.EvKey (V.KChar 's') []  -> do
-                                let (out', vmstate') = stepVM $ vmState state
-                                case out' of
-                                  Just c  -> continue $ state { vmState = vmstate', output = output state <> [c] }
-                                  Nothing -> continue $ state { vmState = vmstate' }
+  V.EvKey (V.KChar 's') []  -> continue state'
   _                         -> continue state
+  where (out', vmstate')  = stepVM $ vmState state
+        state'            = UIState { vmState = vmstate', output = output state <> maybeToList out' }
 eventHandler state _ = continue state
 
 attrMap :: UIState -> A.AttrMap
