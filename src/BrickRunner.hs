74e7ddeaf6e32502f5c4c8438f889fdc8571183f
Fred Strauss <fred@strider.co.za>
2022-03-11 16:49:38 +0200

    Basic memory dump

--- src/BrickRunner.hs
+++ src/BrickRunner.hs
@@ -8,6 +8,8 @@ import Brick.Main
 import Brick.Types
 import Brick.BChan
 import Brick.Widgets.Core
+import Brick.Widgets.Table
+import Brick.Widgets.Center
 import Data.Maybe (maybeToList)
 import Control.Monad (void, forever)
 import Control.Monad.IO.Class (liftIO)
@@ -20,10 +22,12 @@ import qualified Brick.Widgets.Border.Style as Bs
 
 data UIEvent  = StepEvent deriving (Eq, Show)
 data UINames  = OutputView deriving (Eq, Show, Ord)
+data UIMode   = ModeMain | ModeMemory deriving (Eq, Show)
 
 data UIState = UIState
   { vmState :: VMState
   , running :: Bool
+  , uiMode  :: UIMode
   } deriving (Eq, Show)
 
 registersBrick :: UIState -> Widget n
@@ -61,19 +65,34 @@ stackBrick state = vBox $ zipWith (curry (str . show')) [0..] $ take 20 $ vmStac
   where show' (i, v)  = hex i <> " : " <> hex v
 
 hotkeysBrick :: Widget n
-hotkeysBrick = str $ intercalate " | " [ "F2 - Save", "F3 - Load", "F4 - Run", "F5 - Step", "F10 - Quit" ]
+hotkeysBrick = str $ intercalate " | " [ "F2 - Save", "F3 - Load", "F4 - Run", "F5 - Step", "F6 - Memory", "F10 - Quit" ]
 
 inputBrick :: UIState -> Widget n
 inputBrick = str . vmInBuffer . vmState
 
+memoryBrick :: UIState -> Widget n
+memoryBrick state = centerLayer $ B.borderWithLabel (str "Memory") content
+  where cols      = 16
+        content   = vBox $ header : header2 : take 32 (map memrow [0,cols..32751])
+        header    = str $ "     | " <> unwords (map hex (take cols [0..]))
+        header2   = str $ "-----+" <> concat (replicate (cols*5) "-")
+        vmstate   = vmState state
+        readmem i = vmMemory vmstate !! i
+        memrow r  = str $ hex r <> " | " <> unwords (take cols (map (hex . readmem . (+r)) [0..]))
+
 drawUI :: UIState -> [ Widget UINames ]
-drawUI state = [ joinBorders ( (opcodes <+> output' <+> (registers <=> stack) ) <=> hotkeys) ]
-  where output'   = B.border $ viewport OutputView Vertical $ padRight Max $ strWrap $ vmOutput $ vmState state
-        registers = B.border $ registersBrick state
-        opcodes   = B.border $ opCodeBrick state
-        stack     = B.border $ stackBrick state
-        inbuff    = B.border $ inputBrick state
-        hotkeys   = B.border hotkeysBrick
+drawUI state
+  | memui     = [ memoryBrick state, mainlayout ]
+  | otherwise = [ mainlayout ]
+  where mainui      = uiMode state == ModeMain
+        memui       = uiMode state == ModeMemory
+        output'     = B.border $ viewport OutputView Vertical $ padRight Max $ strWrap $ vmOutput $ vmState state
+        registers   = B.border $ registersBrick state
+        opcodes     = B.border $ opCodeBrick state
+        stack       = B.border $ stackBrick state
+        inbuff      = B.border $ inputBrick state
+        hotkeys     = B.border hotkeysBrick
+        mainlayout  = joinBorders ( (opcodes <+> output' <+> (registers <=> stack) ) <=> hotkeys)
 
 eventHandler :: UIState -> BrickEvent UINames UIEvent -> EventM UINames (Next UIState)
 eventHandler state (AppEvent StepEvent)
@@ -85,6 +104,8 @@ eventHandler state (AppEvent StepEvent)
         scroll    = vScrollToEnd (viewportScroll OutputView)
 eventHandler state (VtyEvent e) = case e of
   V.EvKey (V.KFun 10) []  -> halt state
+  V.EvKey (V.KFun 7) []   -> continue state { uiMode = ModeMain }
+  V.EvKey (V.KFun 6) []   -> continue state { uiMode = ModeMemory, running = False }
   V.EvKey (V.KFun 5) []   -> continue state'
   V.EvKey (V.KFun 4) []   -> continue state { running = not $ running state }
   V.EvKey (V.KFun 2) []   -> liftIO (saveVM (vmState state) "vm.state") >> continue state
@@ -111,6 +132,12 @@ app = App { appDraw         = drawUI
           , appAttrMap      = attrMap
           }
 
+uiState :: VMState -> UIState
+uiState state = UIState { vmState = state
+                        , running = False
+                        , uiMode  = ModeMain
+                        }
+
 brickRun :: VMState -> IO VMState
 brickRun state = do
   chan <- newBChan 10
@@ -121,5 +148,5 @@ brickRun state = do
   let buildVty = V.mkVty V.defaultConfig
   initialVty <- buildVty
 
-  uistate' <- customMain initialVty buildVty (Just chan) app $ UIState { vmState = state, running = False }
+  uistate' <- customMain initialVty buildVty (Just chan) app $ uiState state
   return $ vmState uistate'
